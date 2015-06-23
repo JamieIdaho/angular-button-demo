595c0c32cbcff77bbd9a6ab15e50f04fa3c4c985
Jamie Gamblin <jamiemgamblin@gmail.com>
2015-06-23 13:28:07 -0400

    button counter functioning

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,32 @@
+;(function (){
 
+  'use strict';
+
+  angular.module('ButtonDemo', [])
+
+  .controller('ButtonController', ['$scope', function ($scope){
+
+    $scope.allClicks = [];
+
+    var Click = function (count) {
+      this.count = 0;
+
+    };
+
+
+    $scope.clicked = function (x) {
+      var c = new Click();
+      c.count = +1;
+      $scope.allClicks.push(c);
+    };
+
+
+
+    $scope.counter = function (x) {
+      return $scope.allClicks.length;
+
+    };
+
+  }]);
+
+}());
